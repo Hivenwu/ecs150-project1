cc301a9494c2546a3587086e92be1ca6b4b4b3fb
Taspon <tagonggiatgul@ucdavis.edu>
2019-09-30 22:57:47 -0700

    finished phase5.3 (parsing comment argument)

--- mdshell.c
+++ mdshell.c
@@ -5,32 +5,74 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
+struct command {
+    char* args;
+    char* cmd;
+    int retval;
+};
+
+void parse(struct command *object,size_t buffsize) {
+    int num = 0;
+    char* buff;
+    char* line;
+    buff = (char *)malloc(buffsize * sizeof(char));
+
+    for (int index = 0; index < buffsize; index = index + 1) {
+        if ((object->args)[index] != ' ') {
+            buff[num] = object->args[index];
+            num = num + 1;
+        }
+    }
+    
+    strcpy(object->args,buff);
+
+    return;
+}
+
 int main(int argc, char *argv[])
 {
-    char *cmd = "/bin/date";
-    char *date = "date";
-    char *args[] = {cmd, "-u", NULL};
+    char *cmd1 = "/bin/date";
+    char *cmd2 = "/bin/ls";
+    char *args1[] = {cmd1, "-u", NULL};
+    char *args2[] = {cmd2, ".", NULL};
     char *buff;
     char *line;
     int retval;
+    struct command instance;
     size_t buffsize = 40;
     pid_t PID;
 
-    buff = (char *)malloc(buffsize * sizeof(char));
-
     while (1) {
         printf("$sshell: ");
-        getline(&buff,&buffsize,stdin);   //Get user command
+        instance.args = (char *)malloc(buffsize * sizeof(char));
+        getline(&(instance.args),&buffsize,stdin);   //Get user command
+        parse(&instance,buffsize);
 
-        if (strcmp(buff,date)) {   //Accouts for /bin/date -u command
+        if (!strcmp(instance.args,"date\n")) {   //Accouts for /bin/date -u command
             PID = fork();
             if (PID == 0) {
-                retval = execv(cmd,args);
+                retval = execv(cmd1,args1);
             }
             else {
                 wait(&PID);
-                fprintf(stderr, "+ completed '%s': [%d]\n", cmd, retval);
+                fprintf(stderr, "+ completed '%s': [%d]\n", cmd1, retval);
             }
+        }
+
+        else if (!strcmp(instance.args,"ls\n")) {
+            PID = fork();
+            if (PID == 0) {
+                retval = execv(cmd2,args2);
+            }
+            else {
+                wait(&PID);
+                fprintf(stderr, "+ completed '%s': [%d]\n", cmd2, retval);
+            }
+        }
+
+        else {
+            perror("command not found");
         }
     }
 
